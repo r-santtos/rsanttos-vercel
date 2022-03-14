dcdd6bebb565345a337001ab2b0c25d044ba00dd
r-santtos <rsanttos.dev@gmail.com>
2022-03-14 10:35:58 -0300

    finish layout nextjs

---
+++ src/componets/Images.ts
@@ -0,0 +1,8 @@
+const Images = {
+  Profile: require('../../public/images/profile.png').default,
+  Instagram: require('../../public/images/svg/instagram.svg').default,
+  GitHub: require('../../public/images/svg/github.svg').default,
+  Linkedin: require('../../public/images/svg/linkedin.svg').default,
+}
+
+export default Images;
